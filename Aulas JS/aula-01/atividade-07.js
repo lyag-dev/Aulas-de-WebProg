006f358bf96067f337a1f74f8c02fe7e4e36af5a
lyag-dev <lyagushkatribola@gmail.com>
2026-05-28 11:54:03 -0300

    Agora ta funcionando graças ao nicolas neto e o balltrick

--- Aulas JS/aula-01/atividade-07.js
+++ Aulas JS/aula-01/atividade-07.js
@@ -1,17 +1,16 @@
-const prompt = require('prompt-sync')();
 function CalcularResultado() {
-    var nota1 = Number(getElementById("nota1").value)
-    var nota2 = Number(getElementById("nota2").value)
-    var nota3 = Number(getElementById("nota3").value)
+    var nota1 = Number(document.getElementById("nota1").value)
+    var nota2 = Number(document.getElementById("nota2").value)
+    var nota3 = Number(document.getElementById("nota3").value)
     var media = Number((nota1+nota2+nota3)/3)
     let resultado = ""
 
     if (media >= 0 && media < 6) {
-        resultado = String(("O aluno foi reprovado com média:", media))
+        resultado = String(("O aluno foi reprovado com média: " + media))
     }else if (media >= 6 && media < 10) {
-        resultado = String(("O aluno foi aprovado com média:", media))
+        resultado = String(("O aluno foi aprovado com média: " + media))
     }else if (media == 10) {
-        resultado = String(("O aluno foi aprovado com distinção, com a incrível média:", media))
+        resultado = String(("O aluno foi aprovado com distinção, com a incrível média: " + media))
     }else {
         resultado = String(("o valor da média final é inválido, provavelmente devido a um erro no valor das notas."))
     }
@@ -19,6 +18,6 @@ function CalcularResultado() {
     let DivResultado = document.getElementById("resultado")
     DivResultado.style.display = "block";
     DivResultado.innerHTML = `
-    <p><${resultado}</p>
-    `
+    <p>${resultado}</p>
+    `;
 }
