354ee4eea236bb448733a9258b02cc60b73deb1d
lyag-dev <lyagushkatribola@gmail.com>
2026-06-10 22:58:16 -0300

    Atividades 1 a 6 feitas.

---
+++ Aulas JS/aula-02/atividade-06.js
@@ -0,0 +1,30 @@
+function DefinirFxEtaria() {
+    let resultado = ""
+
+    var idade = Number(document.getElementById("idade").value)
+
+    if (idade > 0) {
+        switch (true) {
+            case idade >= 0 && idade < 13:
+                resultado = String("Faixa etária: Criança.")
+                break;
+            case idade >= 13 && idade < 18:
+                resultado = String("Faixa etária: Adolescente.")
+                break;
+            case idade >= 18 && idade < 60:
+                resultado = String("Faixa etária: Adulto.")
+                break;
+            case idade >= 60:
+                resultado = String("O aluno tirou nota F.")
+                break;
+        }
+    } else {
+        resultado = String("idade inválida.")
+    }
+
+    let DivResultado = document.getElementById("resultado")
+    DivResultado.style.display = "block";
+    DivResultado.innerHTML = `
+    <p>${resultado}</p>
+    `;
+}
